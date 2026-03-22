ae252b1e09ca348ea45d7788b7319f5b6287428a
therizzock <dkowalsky7@gmail.com>
2026-03-22 12:39:51 -0400

    coors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from app.routes import lead
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "chrome-extension://oeeefeaddefcngbljhjcjmjlfgkloojn",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(lead.router, prefix="/lead")
