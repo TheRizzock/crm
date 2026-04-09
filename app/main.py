436bb686e51915e315d54f145ba65bc8ac73f335
therizzock <dkowalsky7@gmail.com>
2026-04-08 23:34:49 -0400

    updating for coors

--- app/main.py
+++ app/main.py
@@ -10,6 +10,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "chrome-extension://oeeefeaddefcngbljhjcjmjlfgkloojn",
+        "http://localhost:63343/",
+        "https://dankowalsky.com",  # replace
+        "https://www.dankowalsky.com"  # replace
     ],
     allow_credentials=True,
     allow_methods=["*"],
